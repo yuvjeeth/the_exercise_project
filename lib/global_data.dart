721483b0c2128171ec2688bc12b4bd24af12e659
Yuvjeeth HS <yuvjeeth@gmail.com>
2022-06-22 11:20:06 +0530

    added exercises master list

--- lib/global_data.dart
+++ lib/global_data.dart
@@ -18,6 +18,23 @@ List<String> exerciseVideoUrls = [
 ];
 //List<String> currentWorkout = ["Push-ups","Pull-Ups"];
 
+List<Exercise> exercises = [
+  Exercise(
+      name: exerciseNames[0],
+      imageURL: exerciseImages[0],
+      videoURL: exerciseVideoUrls[0],
+      description: exerciseDescriptions[0],
+      exerciseType: ExerciseType.strength,
+      totalValue: 30),
+  Exercise(
+      name: exerciseNames[1],
+      imageURL: exerciseImages[1],
+      videoURL: exerciseVideoUrls[1],
+      description: exerciseDescriptions[1],
+      exerciseType: ExerciseType.cardio,
+      totalValue: 30),
+];
+
 List<Exercise> currentWorkout = [
   Exercise(
       name: exerciseNames[0],
