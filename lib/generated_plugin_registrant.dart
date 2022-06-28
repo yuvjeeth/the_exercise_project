795acabc433a37b19f7d4e3babb5fd042496a6c0
Yuvjeeth HS <yuvjeeth@gmail.com>
2022-06-28 13:32:42 +0530

    generated plugin registrant

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,12 +6,16 @@
 // ignore_for_file: lines_longer_than_80_chars
 // ignore_for_file: depend_on_referenced_packages
 
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  FluttertoastWebPlugin.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
